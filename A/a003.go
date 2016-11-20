70e6b1c63e188e1663ab10cb67e9e585712043c4
daiyuok <591120400@qq.com>
2016-11-20 09:06:17 +0800

    'm';

--- A/a003.go
+++ A/a003.go
@@ -8,6 +8,7 @@ import "fmt"
  * Time: 22:25
  */
 
+
 type user struct {
 	id   int
 	name string
@@ -24,8 +25,15 @@ func (u user) call() string {
 	return fmt.Sprintf("%+v", u)
 }
 
+func (u *user)area()int  {
+	u.id=2
+	return u.id*u.age
+}
+
 func main() {
 	baba := user{1, "爸爸", 50}
 	f1 := family{baba, 3}
+	fmt.Println(f1.call())
+	fmt.Println(f1.area())
 	fmt.Println(f1.call())
 }
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+/** 
+ * User: daixinyu
+ * Date: 2016/11/20 
+ * Time: 9:05 
+ 
+ */
+
+func main() {
+}
